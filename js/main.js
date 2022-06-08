3a23879504426660936cba543638044579c1e4e4
DariaArtuhova <darianomoko@yandex.ru>
2022-06-08 14:01:37 +0300

    внесла правки

--- js/main.js
+++ js/main.js
@@ -1,3 +1,82 @@
+//Генерируем случайные объекты
+const SIMILAR_OBJECT_COUNT = 25;
+
+const DESC = [
+  'Сидим с Кексом на пляже, учимся делать рандом',
+  'Ловим мух',
+  'Готовимся к полуночным тыг-дык'
+];
+
+const MIN_VALUE_LIKES = 15;
+const MAX_VALUE_LIKES = 200;
+
+const MESSAGE = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+];
+
+const NAME = [
+  'Мария',
+  'Федор',
+  'Иван'
+];
+
+const getRandomValue = (min, max) => Math.round(Math.random() * (max - min) + min);
+
+const mainId = Array.from({length: SIMILAR_OBJECT_COUNT}, (v, i) =>  i + 1);
+
+const photoUrl = Array.from({length: SIMILAR_OBJECT_COUNT}, (v, i) =>  i + 1);
+
+const getRandomItemNoRepeat = (arr) => {
+  const randomElement = getRandomValue(0, arr.length - 1);
+  const randomElementItem = arr[randomElement];
+  arr.splice(randomElement, 1);
+  return randomElementItem;
+};
+
+const getRandomItemRepeat = (elements) => elements[getRandomValue(0, elements.length - 1)];
+
+const shuffle = (array) => {
+  let currentIndex = array.length, temporaryValue, randomIndex;
+  while (0 !== currentIndex) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+  return array;
+};
+
+const idComments = [];
+for (let i = 0; i < SIMILAR_OBJECT_COUNT; i++) {
+  idComments.push(i);
+  shuffle(idComments);
+}
+
+const createComments = () => ({
+  id: getRandomItemNoRepeat(idComments),
+  avatar: `img/${getRandomValue(1, 6)}.svg`,
+  message: getRandomItemRepeat(MESSAGE),
+  name: getRandomItemRepeat(NAME),
+});
+
+const currentComments = Array.from({length: SIMILAR_OBJECT_COUNT}, createComments);
+
+const createObjectPhotos = () => Array.from({length: SIMILAR_OBJECT_COUNT}, () => ({
+  id: getRandomItemNoRepeat(mainId),
+  url: `photos/${getRandomItemNoRepeat(photoUrl)}.svg`,
+  description: getRandomItemRepeat(DESC),
+  likes: getRandomValue(MIN_VALUE_LIKES, MAX_VALUE_LIKES),
+  comments: getRandomItemNoRepeat(currentComments)
+}));
+
+createObjectPhotos();
+
 //Закрытие и открытие модального окна
 const buttonModal = document.querySelector('#upload-file');
 const popupImg = document.querySelector('.img-upload__overlay');
@@ -87,79 +166,3 @@ const getLengthComment = commentInput.oninput = function (textarea, amount) {
   }
 };
 getLengthComment();
-
-//Генерируем случайные объекты
-const getRandomPositiveInteger = (a, b) => {
-  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
-  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
-  const result = Math.random() * (upper - lower + 1) + lower;
-  return Math.floor(result);
-};
-
-const SIMILAR_OBJECT_COUNT = 25;
-
-const DESC = [
-  'Сидим с Кексом на пляже, учимся делать рандом',
-  'Ловим мух',
-  'Готовимся к полуночным тыг-дык'
-];
-
-const MIN_VALUE_LIKES = 15;
-const MAX_VALUE_LIKES = 200;
-
-const MESSAGE = [
-  'Всё отлично!',
-  'В целом всё неплохо. Но не всё.',
-  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
-  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
-  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
-];
-
-const NAME = [
-  'Мария',
-  'Федор',
-  'Иван'
-];
-
-const mainId = Array.from({length: SIMILAR_OBJECT_COUNT}, (v, i) =>  i + 1);
-
-const getRandomArrayElement = (elements) => elements[getRandomPositiveInteger(0, elements.length - 1)];
-
-const shuffle = (array) => {
-  let currentIndex = array.length, temporaryValue, randomIndex;
-  while (0 !== currentIndex) {
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
-    temporaryValue = array[currentIndex];
-    array[currentIndex] = array[randomIndex];
-    array[randomIndex] = temporaryValue;
-  }
-  return array;
-};
-
-const idComments = [];
-for (let i = 0; i < SIMILAR_OBJECT_COUNT; i++) {
-  idComments.push(i);
-  shuffle(idComments);
-}
-
-const createComments = () => ({
-  id: getRandomArrayElement(idComments),
-  avatar: `img/${getRandomPositiveInteger(1, 6)}.svg`,
-  message: getRandomArrayElement(MESSAGE),
-  name: getRandomArrayElement(NAME),
-});
-
-const currentComments = Array.from({length: SIMILAR_OBJECT_COUNT}, createComments);
-
-const createObjectPhotos = () => ({
-  id: getRandomArrayElement(mainId),
-  url: `photos/${getRandomPositiveInteger(1,25)}.svg`,
-  description: getRandomArrayElement(DESC),
-  likes: getRandomPositiveInteger(MIN_VALUE_LIKES, MAX_VALUE_LIKES),
-  comments: getRandomArrayElement(currentComments)
-});
-
-const objectPhotos = Array.from({length: SIMILAR_OBJECT_COUNT}, createObjectPhotos);
-console.log(objectPhotos);
