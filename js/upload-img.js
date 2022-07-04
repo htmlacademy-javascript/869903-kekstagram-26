0b2ca3969d94f9d5d5f5410832ecf3aaaa2a5d2a
DariaArtuhova <darianomoko@yandex.ru>
2022-07-04 21:42:57 +0300

    правки линт

--- js/upload-img.js
+++ js/upload-img.js
@@ -7,11 +7,9 @@ fileChooser.addEventListener('change', () => {
   const file = fileChooser.files[0];
   const fileName = file.name.toLowerCase();
 
-  const matches = FILE_TYPES.some((it) => {
-    return fileName.endsWith(it);
-  });
+  const matches = FILE_TYPES.some((it) =>  fileName.endsWith(it));
 
   if (matches) {
     preview.src = URL.createObjectURL(file);
   }
-})
+});
